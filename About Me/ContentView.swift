89623dc244c469f3c423a469035bb60bd405170f
Yusra Saiyed <yusrasaiyed14@gmail.com>
2023-07-13 13:48:46 -0700

    done

--- About Me/ContentView.swift
+++ About Me/ContentView.swift
@@ -8,19 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var fact1 = ""
+    @State private var fact2 = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack {
+            
+            
+            VStack {
+                Text("about me")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.purple)
+                Text ("Yusra Saiyed")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.red)
+                
+                Button("facts about me"){
+            
+                    fact1 = "i like cats"
+                    
+                    fact2 = "i like pizza"
+                    
+                }//button
+                .font(.title2)
+                .buttonStyle(.borderedProminent)
+                .tint(.purple)
+                
+                Image( "cat" )
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+            
+            } // end of vstack
+            VStack{
+                Text(fact1)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.white)
+                                
+                            Text(fact2)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.white)
+            }
+            
+            
+        } // end of Zstack
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+    
 }
